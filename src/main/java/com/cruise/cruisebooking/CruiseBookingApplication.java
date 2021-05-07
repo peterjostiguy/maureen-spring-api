999ead25acc1ec35df3a6a37826adbb8b9933377
codingfox <carinalxx@gmail.com>
2021-05-07 09:40:18 -0700

    initializr

---
+++ src/main/java/com/cruise/cruisebooking/CruiseBookingApplication.java
@@ -0,0 +1,13 @@
+package com.cruise.cruisebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruiseBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruiseBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cruise/cruisebooking/CruiseBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruise.cruisebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruiseBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
